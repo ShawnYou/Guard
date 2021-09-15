a1a96d79da852c329ae82cdc78e35f74acede5cc
youshuanglin <youshuanglin@migu.cn>
2021-09-15 23:15:48 +0800

    init

---
+++ src/main/java/com/guard/commonguard/CommonGuardApplication.java
@@ -0,0 +1,13 @@
+package com.guard.commonguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guard/commonguard/CommonGuardApplicationTests.java
@@ -0,0 +1,12 @@
+package com.guard.commonguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonGuardApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
